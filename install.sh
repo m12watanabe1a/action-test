aabe3b9c879f27db8efcbb21654b2db6d7e65b29
m12watanabe1a <m12watanabe1a@gmail.com>
2021-11-24 14:19:43 +0900

    fix ssh to https

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Install C driver
 mkdir -p ~/.local/src; cd ~/.local/src
-git clone git@github.com:mongodb/mongo-c-driver.git -b 1.17.7
+git clone https://github.com/mongodb/mongo-c-driver.git -b 1.17.7
 cd mongo-c-driver
 python3 build/calc_release_version.py > VERSION_CURRENT
 mkdir cmake-build; cd cmake-build
@@ -11,7 +11,7 @@ make install
 
 # Install Cxx driver
 cd ~/.local/src
-git clone git@github.com:mongodb/mongo-cxx-driver.git -b r3.6.5
+git clone https://github.com/mongodb/mongo-cxx-driver.git -b r3.6.5
 cd mongo-cxx-driver/build
 cmake -DCMAKE_BUILD_TYPE=Release -DBSONCXX_POLY_USE_BOOST=1 -DCMAKE_PREFIX_PATH=$HOME/.local -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 make -j8
